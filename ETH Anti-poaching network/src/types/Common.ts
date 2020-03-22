eae66ef7a5a62e7593605d9a48377ef256394004
JosephkPaul <50631217+JosephkPaul@users.noreply.github.com>
2020-03-22 09:57:17 +0530

    Add files via upload

---
+++ ETH Anti-poaching network/src/types/Common.ts
@@ -0,0 +1,11 @@
+export type address = string
+
+export declare interface SendOptions {
+  from?: string
+  value?: string | number
+  gas?: string | number
+  gasPrice?: string | number
+  encodeAbi?: boolean
+  parent?: boolean
+  to?: string
+}
